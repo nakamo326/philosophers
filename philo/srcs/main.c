cb09bd8e90990ec9a79c8c47c94c21d7506870c7
Yasufumi Nakamoto <nakamo326@gmail.com>
2021-12-02 17:19:42 +0900

    change file

--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -1,11 +1,30 @@
 #include "philosophers.h"
 
-int	g_test = 9;
-
 int	return_error(char *msg)
 {
 	ft_putstr_fd(msg, STDERR_FILENO);
 	return (EXIT_FAILURE);
+}
+
+bool	parse_arg(t_philo *p, int argc, char **argv)
+{
+	int			i;
+	long long	tmp;
+
+	if (!(argc == 5 || argc == 6))
+		return (false);
+	i = 1;
+	while (i < argc)
+	{
+		if (ft_strlen(argv[i]) > 10 || !ft_isdigits(argv[i]))
+			return (false);
+		tmp = ft_atoll(argv[i]);
+		if (tmp <= 0 || tmp > INT_MAX)
+			return (false);
+		p->params[i] = tmp;
+		i++;
+	}
+	return (true);
 }
 
 int	main(int argc, char **argv)

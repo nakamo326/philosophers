a1330501c83ff0d05bd008e55efbff83b632f135
Yasufumi Nakamoto <nakamo326@gmail.com>
2021-12-04 10:58:20 +0900

    wip

--- philo/srcs/sims.c
+++ philo/srcs/sims.c
@@ -2,9 +2,20 @@
 
 void	*philo_routine(void *philo)
 {
-	t_philo *p;
+	int		i;
+	t_philo	*p;
 
 	p = (t_philo *)philo;
+	i = 0;
+	while (i < p->params[LIMIT_TIMES_TO_DIE])
+	{
+		//check is_dead before every action
+		if(!eat_meal(p))
+			return (p);
+		sleep_well(p->params[TIME_TO_SLEEP]);
+		// think_about_truth(p);?
+
+	}
 
 	return (p);
 }

9d441c8c43e463e80e52f218aa809fc9cc38c06c
Yasufumi Nakamoto <nakamo326@gmail.com>
2021-12-14 00:18:12 +0900

    bonus init

---
+++ philo_bonus/srcs/log.c
@@ -0,0 +1,17 @@
+#include "philosophers.h"
+
+long	output_log(t_philo *p, const char *str)
+{
+	long			time;
+
+	time = get_time();
+	pthread_mutex_lock(&p->info->print);
+	if (!p->info->is_dead
+		&& !(p->info->fullfill_num == p->info->params[NUM_OF_PHILOS]))
+	{
+		printf("%ld %d%s\n", time, p->index, str);
+		printf("%ld %s\n", time, str);
+	}
+	pthread_mutex_unlock(&p->info->print);
+	return (time);
+}

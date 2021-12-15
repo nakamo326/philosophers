05650492625c641341c69f1e7972a2b84c12e62c
Yasufumi Nakamoto <nakamo326@gmail.com>
2021-12-15 09:48:27 +0900

    add new dir

---
+++ philo_bonus/srcs/init.c
@@ -0,0 +1,61 @@
+#include "philosophers.h"
+
+int	exit_free(t_info *info, t_philo *philos, char *err)
+{
+	if (info != NULL)
+	{
+		free(info->forks);
+	}
+	free(philos);
+	if (err != NULL)
+	{
+		printf("%s\n", err);
+		return (EXIT_FAILURE);
+	}
+	return (EXIT_SUCCESS);
+}
+
+bool	init_info(t_info *info)
+{
+	int	i;
+	int	ret;
+
+	info->forks = malloc(sizeof(pthread_mutex_t) * info->params[NUM_OF_PHILOS]);
+	if (info->forks == NULL)
+		return (false);
+	i = 0;
+	while (i < info->params[NUM_OF_PHILOS])
+	{
+		ret = pthread_mutex_init(&info->forks[i], NULL);
+		if (ret != 0)
+			return (false);
+		i++;
+	}
+	if (pthread_mutex_init(&info->print, NULL))
+		return (false);
+	info->is_dead = false;
+	info->fullfill_num = 0;
+	return (true);
+}
+
+t_philo	*init_philos(t_info *info)
+{
+	int		i;
+	int		num;
+	t_philo	*philos;
+
+	num = info->params[NUM_OF_PHILOS];
+	philos = malloc(sizeof(t_philo) * num);
+	i = 0;
+	while (philos != NULL && i < num)
+	{
+		philos[i].info = info;
+		philos[i].index = i + 1;
+		philos[i].left = &info->forks[i];
+		philos[i].right = &info->forks[(i + num - 1) % num];
+		pthread_mutex_init(&philos[i].access_to_last_meal, NULL);
+		philos[i].times_of_finished_meal = 0;
+		i++;
+	}
+	return (philos);
+}
---
+++ philo_bonus/srcs/sims.c
@@ -0,0 +1,35 @@
+#include "philosophers.h"
+
+bool	start_sims(t_philo *philos)
+{
+	int		i;
+	t_philo	*p;
+
+	i = 0;
+	
+	while (i < philos->info->params[NUM_OF_PHILOS])
+	{
+		p = &(philos[i]);
+		if (pthread_create(&p->th, NULL, philo_routine, p)
+			|| pthread_create(&p->doctor, NULL, doctor_routine, p))
+			return (false);
+		i++;
+	}
+	return (true);
+}
+
+void	join_philos(t_philo *philos)
+{
+	int	i;
+	int	num;
+
+	i = 0;
+	num = philos->info->params[NUM_OF_PHILOS];
+	while (i < num)
+	{
+		pthread_join(philos[i].th, NULL);
+		pthread_join(philos[i].doctor, NULL);
+		i++;
+	}
+	return ;
+}
